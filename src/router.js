3373eb45aa896571e6adbfe0a2d9405419b73256
郝甲辰 <haojiachen@ut.cn>
2016-11-24 16:07:01 +0800

    增加homePage

--- src/router.js
+++ src/router.js
@@ -1,21 +1,26 @@
 import React, {PropTypes} from 'react';
 import {Router, Route, IndexRoute, Link, IndexRedirect} from 'dva/router';
+
 import IndexPage from './routes/IndexPage';
 import NotFound from './routes/NotFound';
-
 import LoginPage from './routes/LoginPage';
+import HomePage from './routes/HomePage';
+
+import Example from './components/Example';
+
 
 export default function ({ history }) {
   return (
     <Router history={history}>
-      <Route path="/">
-        <IndexRedirect to="login" />
-        <Route path="app" component={IndexPage}>
-
+      <Route path='/'>
+        <IndexRedirect to='login' />
+        <Route path='app' component={IndexPage}>
+          <IndexRedirect to='home' />
+          <Route path='home' component={HomePage} />
         </Route>
-        <Route path="/login" component={LoginPage} />
+        <Route path='/login' component={LoginPage} />
       </Route>
-      <Route path="*" component={NotFound} />
+      <Route path='*' component={NotFound} />
     </Router>
   );
 };
---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+
+const HomePage = (props) => {
+  console.log(props, 'home');
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+HomePage.propTypes = {};
+
+export default HomePage;
