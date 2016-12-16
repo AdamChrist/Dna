2de348b8943c980ee94f3fcfaf5bb0c63ff9ebb5
郝甲辰 <haojiachen@ut.cn>
2016-12-16 12:42:24 +0800

    设置菜单和权限的初始化数据

--- server/model/Menu.js
+++ server/model/Menu.js
@@ -20,5 +20,22 @@ module.exports = (sequelize, DataTypes) => sequelize.define('menu', {
     associate: ({ Menu, Role, RoleMenu }) => {
       Menu.belongsToMany(Role, { through: { model: RoleMenu, unique: false }, constraints: false })
     }
+  },
+  hooks: {
+    afterSync: async function () {
+      const menus = await this.findAll();
+      //初始化菜单
+      if (menus.length === 0) {
+        const sysMenu = await this.create({ name: '系统管理', code: 'sys', url: '', pid: '', icon: 'laptop', sortNo: 0 });
+        const pid = sysMenu.id;
+        await this.bulkCreate([
+          { name: '用户管理', code: 'user', url: '/app/user', pid: pid, icon: '', sortNo: 0 },
+          { name: '角色管理', code: 'role', url: '/app/role', pid: pid, icon: '', sortNo: 1 },
+          { name: '菜单管理', code: 'menu', url: '/app/menu', pid: pid, icon: '', sortNo: 2 },
+          { name: '权限管理', code: 'rights', url: '/app/rights', pid: pid, icon: '', sortNo: 3 },
+          { name: '数据字典', code: 'dic', url: '/app/dic', pid: pid, icon: '', sortNo: 4 },
+        ]);
+      }
+    }
   }
 });
--- server/model/Rights.js
+++ server/model/Rights.js
@@ -21,5 +21,37 @@ module.exports = (sequelize, DataTypes) => sequelize.define('rights', {
     associate: ({ Rights, Role, RoleRights }) => {
       Rights.belongsToMany(Role, { through: { model: RoleRights, unique: false }, constraints: false });
     }
+  },
+  hooks: {
+    afterSync: async function () {
+      const result = await this.findAll();
+      //初始化
+      if (result.length === 0) {
+        await this.bulkCreate([
+          { name: '用户编辑', code: 'USER/UPDATE', url: '/api/user', method: 'put', type: 'disabled' },
+          { name: '用户新增', code: 'USER/CREATE', url: '/api/user', method: 'post', type: 'hide' },
+          { name: '用户删除', code: 'USER/DEL', url: '/api/user', method: 'delete', type: 'disabled' },
+          { name: '用户重置密码', code: 'USER/PWD', url: '/api/user/pwd', method: 'post', type: 'disabled' },
+          { name: '用户查询', code: 'USER/QUERY', url: '/api/user/query', method: 'post', type: 'hide' },
+          { name: '角色新增', code: 'ROLE/CREATE', url: '/api/role', method: 'post', type: 'hide' },
+          { name: '角色编辑', code: 'ROLE/UPDATE', url: '/api/role', method: 'put', type: 'disabled' },
+          { name: '角色删除', code: 'ROLE/DEL', url: '/api/role', method: 'delete', type: 'disabled' },
+          { name: '角色设置菜单', code: 'ROLE/MENU', url: '/api/role/menu', method: 'post', type: 'disabled' },
+          { name: '角色设置权限', code: 'ROLE/RIGHTS', url: '/api/role/rights', method: 'post', type: 'disabled' },
+          { name: '菜单新增', code: 'MENU/CREATE', url: '/api/menu', method: 'post', type: 'hide' },
+          { name: '菜单修改', code: 'MENU/UPDATE', url: '/api/menu', method: 'put', type: 'disabled' },
+          { name: '菜单删除', code: 'MENU/DEL', url: '/api/menu', method: 'delete', type: 'disabled' },
+          { name: '权限新增', code: 'RIGHTS/CREATE', url: '/api/rights', method: 'post', type: 'hide' },
+          { name: '权限修改', code: 'RIGHTS/UPDATE', url: '/api/rights', method: 'put', type: 'disabled' },
+          { name: '权限删除', code: 'RIGHTS/DEL', url: '/api/rights', method: 'delete', type: 'disabled' },
+          { name: '数据字典类别新增', code: 'DIC/CREATE', url: '/api/dic', method: 'post', type: 'hide' },
+          { name: '数据字典类别修改', code: 'DIC/UPDATE', url: '/api/dic', method: 'put', type: 'hide' },
+          { name: '数据字典类别删除', code: 'DIC/DEL', url: '/api/dic', method: 'delete', type: 'hide' },
+          { name: '数据字典新增', code: 'DICMX/CREATE', url: '/api/dic/mx', method: 'post', type: 'hide' },
+          { name: '数据字典修改', code: 'DICMX/UPDATE', url: '/api/dic/mx', method: 'put', type: 'disabled' },
+          { name: '数据字典删除', code: 'DICMX/DEL', url: '/api/dic/mx', method: 'delete', type: 'disabled' },
+        ]);
+      }
+    }
   }
 });
