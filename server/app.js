b0f6e07f6f5f14c0996473289c06957dde587167
郝甲辰 <haojiachen@ut.cn>
2016-12-16 16:57:25 +0800

    去掉打包命令

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 // 设置默认环境变量
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 //兼容es6
-if (process.env.NODE_ENV == 'development') {
-  require('babel-register')({ presets: ['es2015', 'stage-0'] });
-}
+require('babel-register')({ presets: ['es2015', 'stage-0'] });
 require('babel-polyfill');
 
 const express = require('express');
